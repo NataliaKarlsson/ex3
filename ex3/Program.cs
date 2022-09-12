ec4f97def4046651016e65c1c6d798142e4aace1
NataliaKarlsson <112874590+NataliaKarlsson@users.noreply.github.com>
2022-09-12 17:57:40 +0200

    Add project files.

---
+++ ex3/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ex3
+{
+    internal class Program
+    {
+        
+        
+            static void Main(string[] args)
+            {
+                //Console.Write("Enter the width and height of an image");
+                Console.Write("Enter the width of an image: ");
+
+                string? widthStr = Console.ReadLine();
+                int widthInt = Convert.ToInt32(widthStr);
+
+                Console.Write("Enter the height of an image: ");
+
+                string? heightStr = Console.ReadLine();
+                int heightInt = Convert.ToInt32(heightStr);
+
+                if (widthInt > heightInt)
+                {
+                    Console.Write("The image is landscape");
+                }
+                else
+                {
+                    Console.Write("The image is portrait");
+                }
+
+            }
+
+        
+    }
+}
